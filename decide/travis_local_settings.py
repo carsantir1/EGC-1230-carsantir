1dcad704c0ccb61fce03dbed26ef00fc153d27b6
carsantir1 <csantostirado99@gmail.com>
2021-02-12 11:44:23 +0100

    heroku continuo

---
+++ decide/travis_local_settings.py
@@ -0,0 +1,44 @@
+Luego ir a travis-ci.com y en settings del repositorio, bajar y en variables de entorno añadir:
+HEROKU_AUTH_TOKEN y el valor de la clave
+3) Ahora, crear archivo dentro de decide/ que sea travis_local_settings.py con el siguiente código:
+ALLOWED_HOSTS = ["*"]
+
+# Modules in use, commented modules that you won't use
+MODULES = [
+    'authentication',
+    'base',
+    'booth',
+    'census',
+    'mixnet',
+    'postproc',
+    'store',
+    'visualizer',
+    'voting',
+]
+
+APIS = {
+    'authentication': 'http://localhost:8000',
+    'base': 'http://localhost:8000',
+    'booth': 'http://localhost:8000',
+    'census': 'http://localhost:8000',
+    'mixnet': 'http://localhost:8000',
+    'postproc': 'http://localhost:8000',
+    'store': 'http://localhost:8000',
+    'visualizer': 'http://localhost:8000',
+    'voting': 'http://localhost:8000',
+}
+
+BASEURL = 'http://localhost:8000'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+# number of bits for the key, all auths should use the same number of bits
+KEYBITS = 256
